94fc714dcb390d6f11a157c145663c70bf0d192f
Jaloliddin7 <abdutolipovjaloliddin913@gmail.com>
2023-04-08 03:23:02 +0500

    end

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,60 @@
 const inputEl = document.querySelector('input')
 const form = document.querySelector('#form')
+const select = document.querySelector('#select')
 const countriesEL = document.querySelector('#countries')
+const wrap = document.querySelector(".wrap")
+
 
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    getData(inputEl.value);
+  e.preventDefault()
+  getData(inputEl.value);
 })
 
 let countries = []
 
 async function getData(name) {
-    if (name) {
-        const res = await fetch(`https://restcountries.com/v3.1/name/${name}`)
-        const data = await res.json()
+  wrap.classList.toggle("hidden")
+  if (name) {
+    const res = await fetch(`https://restcountries.com/v3.1/name/${name}`)
+    const data = await res.json()
 
-        countries = data;
-    } else {
-        const res = await fetch(`https://restcountries.com/v3.1/all`)
-        const data = await res.json()
+    countries = data;
+  } else {
+    const res = await fetch(`https://restcountries.com/v3.1/all`)
+    const data = await res.json()
 
-        countries = data;
-    }
-    upDateUI()
+    countries = data;
+  }
+  upDateUI()
+  wrap.classList.toggle("hidden")
 }
+
+// get Region
+async function getByRegion(region) {
+  wrap.classList.toggle("hidden")
+  if (region) {
+    const res = await fetch(`https://restcountries.com/v3.1/region/${region}`)
+    const data = await res.json()
+
+    countries = data;
+  }
+  upDateUI()
+  wrap.classList.toggle("hidden")
+}
+
+select.addEventListener("change" , (e)=>{
+    const region = e.target.value;
+    getByRegion(region)
+})
 
 
 function upDateUI() {
 
-    let html = ""
+  let html = ""
 
-    countries.forEach(country => {
-        html += `
+  countries.forEach(country => {
+    html += `
         <div onclick="linkCountry('${country.name.common}')" class="bg-white dark:bg-[#2B3844] cursor-pointer  card-shadow w-[264px]">
           <img src=${country.flags.png} alt="flag.img">
           <div class="p-6">
@@ -47,16 +70,16 @@ function upDateUI() {
           </div>
         </div>
         `
-    })
+  })
 
-    countriesEL.innerHTML = html;
+  countriesEL.innerHTML = html;
 }
 
 getData()
 
 
 
-function linkCountry(name){
+function linkCountry(name) {
   location.href = `country.html?nomi=${name}`
 }
 
@@ -79,19 +102,21 @@ if (local) {
   Dark()
 }
 
-function click(el){
+function click(el) {
   if (el == dark) {
     el.addEventListener('click', () => {
-        Dark()
-        localStorage.setItem("key", "")
+      Dark()
+      localStorage.setItem("key", "")
     })
-} else if (el == light) {
+  } else if (el == light) {
     el.addEventListener('click', () => {
-        Dark()
-        localStorage.setItem("key", "dark")
+      Dark()
+      localStorage.setItem("key", "dark")
     })
-} 
+  }
 }
 
 click(dark)
 click(light)
+
+
--- src/js/country.js
+++ src/js/country.js
@@ -1,4 +1,5 @@
 const country = document.querySelector("#country")
+const wrap = document.querySelector(".wrap")
 const queryString = window.location.search;
 
 const urlParams = new URLSearchParams(queryString)
@@ -7,64 +8,114 @@ const country_name = urlParams.get("nomi")
 
 
 
-async function getContry(name){
+async function getContry(name) {
+    wrap.classList.toggle("hidden")
     const req = await fetch(`https://restcountries.com/v3.1/name/${name}`)
 
     const data = await req.json()
 
     if (data.length > 0) {
-        const info = data[0]
+        const { borders, population, name, region, subregion, capital, tld, languages, flags, currencies } = data[0]
 
-        for (const border of info.borders) {
-            const req = await fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+        let borderNames = []
+        if (borders) {
+            for (const border of borders) {
+                const req = await fetch(`https://restcountries.com/v3.1/alpha/${border}`)
 
-            let borderNames = []
-            const data = await req.json()
-            borderNames.push(data.name.common)
-            
+                const data = await req.json()
+                borderNames.push(data[0].name.common)
+
+            }
         }
+
+        let leng = Object.values(languages)
         console.log(data);
-        let  el = ` 
-        <img src=${info.flags.png} alt="" width="560" height="401">
+        console.log(leng);
+        let el = ` 
+        <img src=${flags.png} alt="" width="560" height="401">
 
         <div >
-            <h1 class=" font-bold text-3xl mb-6">${info.name.common}</h1>
+            <h1 class=" font-bold text-3xl mb-6">${name.common}</h1>
             <div class="flex justify-between gap-36 mb-16">
                 <ul class="flex flex-col gap-2">
                     <li class="txet-sm font-semibold">
-                        Native Name: <span class="opacity-75">${info}</span></li>
+                        Native Name: <span class="opacity-75">${Object.values(name.nativeName)[0].official}</span></li>
                     <li class="txet-sm font-semibold">
-                        Population: <span class="opacity-75">${info.population}</span></li>
+                        Population: <span class="opacity-75">${population}</span></li>
                     <li class="txet-sm font-semibold">
-                        Region: <span class="opacity-75"> ${info.region}</span></li>
+                        Region: <span class="opacity-75"> ${region}</span></li>
                     <li class="txet-sm font-semibold">
-                        Sub Region: <span class="opacity-75">${info.subregion}</span></li>
+                        Sub Region: <span class="opacity-75">${subregion}</span></li>
                     <li class="txet-sm font-semibold">
-                        Capital: <span class="opacity-75">${info.capital}</span></li>
+                        Capital: <span class="opacity-75">${capital}</span></li>
                   </ul>
                 <ul class="flex flex-col gap-2">
                     <li class="txet-sm font-semibold">
-                        Top Level Domain: <span class="opacity-75">${info.tld}</span></li>
+                        Top Level Domain: <span class="opacity-75">${tld}</span></li>
                     <li class="txet-sm font-semibold">
-                        Currencies: <span class="opacity-75">${info}</span></li>"*/
+                        Currencies: <span class="opacity-75">${Object.values(currencies)[0].name}</span></li>
                     <li class="txet-sm font-semibold">
-                        Languages: <span class="opacity-75">${info.languages.eng}</span></li>
+                        Languages: <span class="opacity-75">${leng}</span></li>
                   </ul>
                 </div>
-                <div class="flex gap-4">
+                <div class="flex gap-4 ">
                   <h1 class="font-semibold">Border Countries: </h1>
-                  <ul class="flex gap-2.5
-                  ">
-                      <li class="py-1 px-7 shadow text-sm cursor-pointer">${info.languages.eng}</li>
+                  <ul class="flex gap-2.5 max-w-sm flex-wrap">
+                  ${borderNames
+                .map(
+                    border => `<a href= "country.html?nomi=${border}" class="py-1 px-7 shadow dark:bg-[#2B3844] text-sm cursor-pointer">${border}</a>`
+                )
+            }
                   </ul>
                 </div>
         </div>
         `
+        country.innerHTML = el
+        wrap.classList.toggle("hidden")
+
     }
 
-
-    
-   
 }
 
 getContry(country_name)
+
+
+
+const dark = document.querySelector("#dark")
+const light = document.querySelector("#light")
+const arrow = document.querySelector("#arrow")
+const arrowDark = document.querySelector("#arrowDark")
+
+const html = document.querySelector("html")
+
+const local = localStorage.getItem("key")
+
+// Theme
+const Dark = () => {
+    light.classList.toggle('hidden')
+    dark.classList.toggle('hidden')
+    arrow.classList.toggle('hidden')
+    arrowDark.classList.toggle('hidden')
+    html.classList.toggle("dark")
+}
+
+if (local) {
+    Dark()
+}
+
+function click(el) {
+    if (el == dark) {
+        el.addEventListener('click', () => {
+            Dark()
+            localStorage.setItem("key", "")
+        })
+    } else if (el == light) {
+        el.addEventListener('click', () => {
+            Dark()
+            localStorage.setItem("key", "dark")
+        })
+    }
+}
+
+click(dark)
+click(light)
